6c0b46bce46088e2587c2735163ed1ef4b9971b4
joyran <2574202043@qq.com>
2023-12-19 17:44:18 +0800

    完善elevator的实现

--- elevator.h
+++ elevator.h
@@ -1,29 +1,37 @@
 #ifndef ELEVAOTR_H
 #define ELEVAOTR_H
 #define MAXPEOPLE 12
+#define MaxFloor 14
 #include "list_basic.h"
-
-class Elevator : public LinkList{
+#include "people.h"
+#include "floor.h"
+class Elevator {
 public:
     Elevator();
     ~Elevator();
     void SetFloor(int floor);
     int GetFloor();
-    void SetDirection(int direction);
-    int GetDirection();
     void SetState(int state);
     int GetState();
     void SetPeopleNum(int peopleNum);
     int GetPeopleNum();
+    int getDirection();
+    People* GetTargetFloor();
+
+
+    void addPeople(People people,People* TargetFloor); //添加乘客
+    void deletePeople(People* TargetFloor); //删除乘客
+    void sortedTargetFloor(int currentFloor,int direction,People* TargetFloor);
+    void SetIntTargetFloor(Floor floor[14]);
+
 
 
 private:
-    int floor;
-    int direction;
-    int state;
+    int currentFloor;
+    int state;     //0表示电梯静止，1表示电梯上升，2表示电梯下降
     int peopleNum;
-    int TargetFloor[14];
-
+    int direction; //1表示电梯上升，2表示电梯下降
+    People TargetFloor[14];
 
 };
 
